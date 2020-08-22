167c7fb8af7faba17a153712303866682bef5c36
ManuelDiu <63983711+ManuelDiu@users.noreply.github.com>
2020-08-22 17:41:56 -0300

    Trabajo Práctico BD World

---
+++ BD_World/frmWorld.Designer.vb
@@ -0,0 +1,196 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmWorld
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtCantPaises = New System.Windows.Forms.TextBox()
+        Me.txtCantCiudades = New System.Windows.Forms.TextBox()
+        Me.txtCantIdiomas = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.txtBuscar = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.grdListado = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.grdListado, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(38, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(237, 29)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Base de datos World"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(46, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Cant. Paises"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(30, 120)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(99, 16)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Cant. Ciudades"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(40, 160)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(89, 16)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Cant. Idiomas"
+        '
+        'txtCantPaises
+        '
+        Me.txtCantPaises.Cursor = System.Windows.Forms.Cursors.No
+        Me.txtCantPaises.Enabled = False
+        Me.txtCantPaises.Location = New System.Drawing.Point(135, 77)
+        Me.txtCantPaises.Name = "txtCantPaises"
+        Me.txtCantPaises.ReadOnly = True
+        Me.txtCantPaises.Size = New System.Drawing.Size(51, 20)
+        Me.txtCantPaises.TabIndex = 4
+        '
+        'txtCantCiudades
+        '
+        Me.txtCantCiudades.Cursor = System.Windows.Forms.Cursors.No
+        Me.txtCantCiudades.Enabled = False
+        Me.txtCantCiudades.Location = New System.Drawing.Point(135, 119)
+        Me.txtCantCiudades.Name = "txtCantCiudades"
+        Me.txtCantCiudades.ReadOnly = True
+        Me.txtCantCiudades.Size = New System.Drawing.Size(51, 20)
+        Me.txtCantCiudades.TabIndex = 5
+        '
+        'txtCantIdiomas
+        '
+        Me.txtCantIdiomas.Cursor = System.Windows.Forms.Cursors.No
+        Me.txtCantIdiomas.Enabled = False
+        Me.txtCantIdiomas.Location = New System.Drawing.Point(135, 160)
+        Me.txtCantIdiomas.Name = "txtCantIdiomas"
+        Me.txtCantIdiomas.ReadOnly = True
+        Me.txtCantIdiomas.Size = New System.Drawing.Size(51, 20)
+        Me.txtCantIdiomas.TabIndex = 6
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnBuscar)
+        Me.GroupBox1.Controls.Add(Me.txtBuscar)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.grdListado)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 195)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(527, 420)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Location = New System.Drawing.Point(327, 20)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(75, 23)
+        Me.btnBuscar.TabIndex = 10
+        Me.btnBuscar.Text = "Buscar"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'txtBuscar
+        '
+        Me.txtBuscar.Location = New System.Drawing.Point(140, 22)
+        Me.txtBuscar.Name = "txtBuscar"
+        Me.txtBuscar.Size = New System.Drawing.Size(171, 20)
+        Me.txtBuscar.TabIndex = 9
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(35, 23)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(99, 16)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Buscar Ciudad:"
+        '
+        'grdListado
+        '
+        Me.grdListado.AllowUserToAddRows = False
+        Me.grdListado.AllowUserToDeleteRows = False
+        Me.grdListado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grdListado.Location = New System.Drawing.Point(31, 60)
+        Me.grdListado.Name = "grdListado"
+        Me.grdListado.ReadOnly = True
+        Me.grdListado.Size = New System.Drawing.Size(459, 340)
+        Me.grdListado.TabIndex = 0
+        '
+        'frmWorld
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(551, 627)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.txtCantIdiomas)
+        Me.Controls.Add(Me.txtCantCiudades)
+        Me.Controls.Add(Me.txtCantPaises)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmWorld"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "World"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.grdListado, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtCantPaises As TextBox
+    Friend WithEvents txtCantCiudades As TextBox
+    Friend WithEvents txtCantIdiomas As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents txtBuscar As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents grdListado As DataGridView
+End Class
---
+++ BD_World/frmWorld.vb
@@ -0,0 +1,82 @@
+﻿Imports System.Linq.Expressions
+Imports MySql.Data.MySqlClient
+Public Class frmWorld
+    Dim conexion As New MySqlConnection("server=localhost; database=world; Uid=root; Pwd=; ")
+    Dim adaptador As New MySqlDataAdapter
+    Dim ds As New DataSet
+    Dim cmd As New MySqlCommand
+    Dim dt As DataTable
+
+
+    Sub refrescarGrd()
+        Dim consulta As String
+        consulta = "SELECT country.Name AS 'Pais', city.Name AS 'Ciudad', country.Population AS 'Población', country.IndepYear AS 'Año Independencia' FROM country, city"
+        adaptador = New MySqlDataAdapter(consulta, conexion)
+        ds = New DataSet
+        adaptador.Fill(ds, "world")
+        grdListado.DataSource = ds.Tables(0)
+    End Sub
+
+    Dim consult As New MySqlCommand("SELECT COUNT(Name) FROM country", conexion)
+
+    Private Sub cantPaises()
+        Dim dr As MySqlDataReader
+        Dim cantPaises As String = "SELECT COUNT(Name) FROM country"
+        cmd = New MySqlCommand(cantPaises, conexion)
+        dr = cmd.ExecuteReader
+        dr.Read()
+        txtCantPaises.Text = dr(0)
+        dr.Close()
+    End Sub
+
+    Private Sub cantCiudades()
+        Dim dr As MySqlDataReader
+        Dim cantPaises As String = "SELECT COUNT(Name) FROM city"
+        cmd = New MySqlCommand(cantPaises, conexion)
+        dr = cmd.ExecuteReader
+        dr.Read()
+        txtCantCiudades.Text = dr(0)
+        dr.Close()
+    End Sub
+
+    Private Sub cantIdiomas()
+        Dim dr As MySqlDataReader
+        Dim cantPaises As String = "SELECT COUNT(Language) FROM countrylanguage"
+        cmd = New MySqlCommand(cantPaises, conexion)
+        dr = cmd.ExecuteReader
+        dr.Read()
+        txtCantIdiomas.Text = dr(0)
+        dr.Close()
+    End Sub
+
+    Public Sub Filtrar(Valor As String)
+        Dim consultafiltrar As String = "SELECT country.Name AS 'Pais', city.Name AS 'Ciudad', country.Population AS 'Población', country.IndepYear AS 'Año Independencia' FROM country, city WHERE city.name LIKE '%" & Valor & "%'"
+        Dim cmd As New MySqlCommand(consultafiltrar, conexion)
+        Dim dt As New DataTable()
+        Dim da As New MySqlDataAdapter(cmd)
+
+        da.Fill(dt)
+
+        grdListado.DataSource = dt
+    End Sub
+
+    Private Sub frmWorld_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            conexion.Open()
+            refrescarGrd()
+            cantPaises()
+            cantCiudades()
+            cantIdiomas()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        Try
+            Filtrar(txtBuscar.Text)
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Class
